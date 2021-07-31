e5a4e7b36dcf3759161afe1f215f0c7c3cd37c3a
Anri Liy <amedamachip713@gmail.com>
2021-07-30 20:27:42 -0700

    Title, Style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from "prop-types";
+
+const Header = ({ title }) => {
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <button className="btn">Add</button>
     </header>
   )
 }
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = {
+//   color: 'red', 
+//   backgroundColor: 'blue'
+// }
 
 export default Header
