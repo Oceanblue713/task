489f9e4440d88f4280ab49e8e0c62d2469160dbb
Anri Liy <amedamachip713@gmail.com>
2021-08-24 21:06:17 -0700

    use Effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false);
 
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json();
+
+      console.log(data);
+    }
+
+    fetchTasks();
+  }, []);
 
   // Add Task
   const addTask = (task) => {
